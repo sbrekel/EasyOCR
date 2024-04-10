28a4d7e2352e8062027d36d26a790e91559a8297
sbrekel <122797589+sbrekel@users.noreply.github.com>
2024-04-10 09:54:12 +0100

    Update utils.py

--- easyocr/utils.py
+++ easyocr/utils.py
@@ -863,7 +863,7 @@ def set_result_with_confidence_rotation(results):
         # Take the row_ix associated with the max confidence
         confidence_vals = [(row_ix, results[row_ix][col_ix][2]) for row_ix in range(len(results))]
         #Introduce bias towards original rotation
-        confidence_vals[0] = (0, results[0][col_ix][2]+0.2)
+        confidence_vals[0] = (0, results[0][col_ix][2]+0.3)
         best_row = max(confidence_vals,key=lambda x: x[1])[0]
         final_result.append(results[best_row][col_ix])
 
